fb9c3db4093b5eba7883f9dec80c7a0a98391344
Curtis Maddalozzo <cmaddalozzo@gmail.com>
2015-11-30 12:58:37 -0800

    Add simple deploy script.

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
-rm -rf dist/*
-zip -r dist/ProcessImages.zip processimages/*
+BASE=$PWD
+rm -rf ${BASE}/dist/*
+cd ${BASE}/processimages && zip -r ${BASE}/dist/ProcessImages.zip *
+cd ${BASE}/deletecopies && zip -r ${BASE}/dist/DeleteCopies.zip *
+cd ${BASE}
 aws lambda update-function-code \
   --function-name ProcessImages \
   --zip-file fileb://dist/ProcessImages.zip
+aws lambda update-function-code \
+  --function-name DeleteCopies \
+  --zip-file fileb://dist/DeleteCopies.zip
