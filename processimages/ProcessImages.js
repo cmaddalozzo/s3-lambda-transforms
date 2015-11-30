12943b2dbf2a7c705350ee42595ac7f0fd00fb74
Curtis Maddalozzo <cmaddalozzo@gmail.com>
2015-11-30 13:43:09 -0800

    Refactor this to remove a lot of duplicate code.

--- processimages/ProcessImages.js
+++ processimages/ProcessImages.js
@@ -1,13 +1,8 @@
-// dependencies
 var async = require('async');
 var AWS = require('aws-sdk');
 var gm = require('gm')
             .subClass({ imageMagick: true }); // Enable ImageMagick integration.
-var util = require('util');
 
-// constants
-var MAX_WIDTH  = 100;
-var MAX_HEIGHT = 100;
 var sizes = {
   small: {
     width: 300
@@ -20,13 +15,12 @@ var sizes = {
   }
 };
 
-// get reference to S3 client 
 var s3 = new AWS.S3();
  
 exports.handler = function(event, context) {
   "use strict";
   // Read options from the event.
-  console.log('Starting image processing.');
+  console.log('Starting image processing...');
   var srcBucket = event.Records[0].s3.bucket.name;
   // Object key may have spaces or unicode non-ASCII characters.
   var srcKey =
@@ -37,6 +31,7 @@ exports.handler = function(event, context) {
     return;
   }
 
+  // This the file path without orig/ at the beginning.
   var outKey = srcKey.match(/^orig\/(.*)/)[1];
 
   // Infer the image type.
@@ -50,7 +45,7 @@ exports.handler = function(event, context) {
     console.log('Skipping non-image: ' + srcKey);
     return;
   }
-  // Download the image from S3, transform, and upload to a different S3 bucket.
+  // Download the image from S3, transform, and upload copies.
   async.waterfall([
     function download(next) {
       // Download the image from S3 into a buffer.
@@ -61,64 +56,34 @@ exports.handler = function(event, context) {
         next);
     },
     function transform(response, next) {
-      async.parallel({
-        small: function(cb){
-          gm(response.Body).size(function(err, size) {
-            // Infer the scaling factor to avoid stretching the image unnaturally.
-            var scalingFactor = sizes.small.width / size.width;
-            var width  = scalingFactor * size.width;
-            var height = scalingFactor * size.height;
-            // Transform the image buffer in memory.
-            this.resize(width, height)
-              .toBuffer(imageType, function(err, buffer) {
-                if (err) {
-                  cb(err);
-                } else {
-                  cb(null, {contentType: response.ContentType, data: buffer});
-                }
-              });
-          });
-        },
-        medium: function(cb){
-          gm(response.Body).size(function(err, size) {
-            // Infer the scaling factor to avoid stretching the image unnaturally.
-            var scalingFactor = sizes.medium.width / size.width;
-            var width  = scalingFactor * size.width;
-            var height = scalingFactor * size.height;
-            // Transform the image buffer in memory.
-            this.resize(width, height)
-              .toBuffer(imageType, function(err, buffer) {
-                if (err) {
-                  cb(err);
-                } else {
-                  cb(null, {contentType: response.ContentType, data: buffer});
-                }
-              });
-          });
-        },
-        large: function(cb){
-          gm(response.Body).size(function(err, size) {
-            // Infer the scaling factor to avoid stretching the image unnaturally.
-            var scalingFactor = sizes.large.width / size.width;
-            var width  = scalingFactor * size.width;
-            var height = scalingFactor * size.height;
-            // Transform the image buffer in memory.
-            this.resize(width, height)
-              .toBuffer(imageType, function(err, buffer) {
-                if (err) {
-                  cb(err);
-                } else {
-                  cb(null, {contentType: response.ContentType, data: buffer});
-                }
-              });
-          });
-        }
-      }, function(err, results){
-        next(null, results);
+      var results = {};
+      async.forEachOf(sizes, function(dimensions, sizeName, cb){
+        console.log('Processing ' + sizeName);
+        gm(response.Body).size(function(err, size) {
+          // Infer the scaling factor to avoid stretching the image unnaturally.
+          var scalingFactor = dimensions.width / size.width;
+          var width  = scalingFactor * size.width;
+          var height = scalingFactor * size.height;
+          // Transform the image buffer in memory.
+          this.resize(width, height)
+            .toBuffer(imageType, function(err, buffer) {
+              if (err) {
+                cb(err);
+              } else {
+                results[sizeName] = {
+                  contentType: response.ContentType, 
+                  data: buffer
+                };
+                cb();
+              }
+            });
+        });
+      }, function(err){
+        next(err, results);
       });
     },
     function upload(images, next) {
-      // Stream the transformed image to a different S3 bucket.
+      // Stream the transformed image to the appropriate place in this bucket.
       async.forEachOf(images, function(image, size,  cb){
         s3.putObject({
             Bucket: event.Records[0].s3.bucket.name,
